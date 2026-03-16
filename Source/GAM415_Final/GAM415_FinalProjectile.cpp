8ece83e5bd15e601b5bc70220af9adfde32e99eb
Ina940 <joyce.dyer@snhu.edu>
2026-03-16 19:21:55 -0400

    Added niagara particle effect to weapon projectile.

--- Source/GAM415_Final/GAM415_FinalProjectile.cpp
+++ Source/GAM415_Final/GAM415_FinalProjectile.cpp
@@ -6,6 +6,8 @@
 #include "Components/SphereComponent.h"
 #include "Components/DecalComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
 
 AGAM415_FinalProjectile::AGAM415_FinalProjectile() 
 {
@@ -64,6 +66,14 @@ void AGAM415_FinalProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherA
 	// sets the randomized colors of the decal when the projectile hits something
 	if (OtherActor != nullptr)
 	{
+		// adds the niagara particle system information needed to spawn the particles and destroy the ball component
+		if (colorP)
+		{
+			UNiagaraComponent* particleComp = UNiagaraFunctionLibrary::SpawnSystemAttached(colorP, HitComp, NAME_None, FVector(-20.f, 0.f, 0.f), FRotator(0.f), EAttachLocation::KeepRelativeOffset, true);
+			particleComp->SetNiagaraVariableLinearColor(FString("RandomColor"), randColor);
+			ballMesh->DestroyComponent();
+			CollisionComp->BodyInstance.SetCollisionProfileName("NoCollision");
+		}
 		float frameNum = UKismetMathLibrary::RandomFloatInRange(0.f, 3.f);
 
 		auto Decal = UGameplayStatics::SpawnDecalAtLocation(GetWorld(), baseMat, FVector(UKismetMathLibrary::RandomFloatInRange(20.f, 40.f)), Hit.Location, Hit.Normal.Rotation(), 0.f);
