3ee7d40872232a9ed6670812ea7894e39d8388b3
Ina940 <joyce.dyer@snhu.edu>
2026-04-03 23:27:28 -0400

    Added portals

---
+++ Source/GAM415_Final/Portal.cpp
@@ -0,0 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Portal.h"
+#include "GAM415_FinalCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+APortal::APortal()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	boxComp = CreateDefaultSubobject<UBoxComponent>("Box Comp");
+	sceneCapture = CreateDefaultSubobject<USceneCaptureComponent2D>("Capture");
+	rootArrow = CreateDefaultSubobject<UArrowComponent>("Root Arrow");
+
+	// sets component attachments
+	RootComponent = boxComp;
+	mesh->SetupAttachment(boxComp);
+	sceneCapture->SetupAttachment(mesh);
+	rootArrow->SetupAttachment(RootComponent);
+
+	// disables collision so we can walk through and trigger overlap event
+	mesh->SetCollisionResponseToAllChannels(ECR_Ignore);
+
+}
+
+// Called when the game starts or when spawned
+void APortal::BeginPlay()
+{
+	Super::BeginPlay();
+	boxComp->OnComponentBeginOverlap.AddDynamic(this, &APortal::OnOverlapBegin);
+	mesh->SetHiddenInSceneCapture(true);
+
+	if (mat)
+	{
+		mesh->SetMaterial(0, mat);
+	}
+	
+}
+
+// Called every frame
+void APortal::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	UpdatePortals();
+
+}
+
+void APortal::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AGAM415_FinalCharacter* playerChar = Cast<AGAM415_FinalCharacter>(OtherActor);
+
+	// checks if cast to player character is valid
+	if (playerChar)
+	{
+		// checks if other portal variable is valid
+		if (OtherPortal)
+		{
+			// checks to see if isTeleporting is false
+			if (!playerChar->isTeleporting)
+			{
+				playerChar->isTeleporting = true;
+				FVector loc = OtherPortal->rootArrow->GetComponentLocation();
+				playerChar->SetActorLocation(loc);
+
+				// sets timer to prevent infinite looping
+				FTimerHandle TimerHandle;
+				FTimerDelegate TimerDelegate;
+				TimerDelegate.BindUFunction(this, "SetBool", playerChar);
+				GetWorld()->GetTimerManager().SetTimer(TimerHandle, TimerDelegate, 1, false);
+			}
+		}
+	}
+}
+
+void APortal::SetBool(AGAM415_FinalCharacter* playerChar)
+{
+	playerChar->isTeleporting = false;
+}
+
+void APortal::UpdatePortals()
+{
+	// moves where player character is while referencing the other portal
+	FVector Location = this->GetActorLocation() - OtherPortal->GetActorLocation();
+	FVector camLocation = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0)->GetTransformComponent()->GetComponentLocation();
+	FRotator camRotation = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0)->GetTransformComponent()->GetComponentRotation();
+	FVector CombineLocation = camLocation + Location;
+
+	sceneCapture->SetWorldLocationAndRotation(CombineLocation, camRotation);
+}
+
---
+++ Source/GAM415_Final/Portal.h
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Engine/TextureRenderTarget2D.h"
+#include "Components/BoxComponent.h"
+#include "Components/SceneCaptureComponent2D.h"
+#include "GAM415_FinalCharacter.h"
+#include "Components/ArrowComponent.h"
+#include "Portal.generated.h"
+
+// lets us access player character
+class GAM415_FinalCharacter;
+
+UCLASS()
+class GAM415_FINAL_API APortal : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APortal();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USceneCaptureComponent2D* sceneCapture;
+
+	UPROPERTY(EditAnywhere)
+	UArrowComponent* rootArrow;
+
+	// sets an opposite target for an opposite portal
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTextureRenderTarget2D* renderTarget;
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* boxComp;
+
+	// allows us to sec which portal are connected
+	UPROPERTY(EditAnywhere)
+	APortal* OtherPortal;
+
+	UPROPERTY(EditAnywhere)
+	UMaterialInterface* mat;
+
+	// creates overlap event to allow for teleporting
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void SetBool(AGAM415_FinalCharacter* playerChar);
+
+	UFUNCTION()
+	void UpdatePortals();
+
+};
